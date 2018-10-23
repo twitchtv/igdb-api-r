3c7cdf0b7688c83869e5c4ed6bdb2893f04ea3fb
husnjak <husnjak@student.chalmers.se>
2018-10-23 16:00:08 +0200

    updated wrapper

--- R/Wrapper.R
+++ R/Wrapper.R
@@ -2,19 +2,18 @@ library(httr)
 library(jsonlite)
 library(roxygen2)
 
-igdb_key = "" #Set User-Key
-
 #' igdb_request requests the IGDB API for information
 #'
 #' @param endpoint   The endpoint you wish to request, use the pre-made endpoint varaiables
 #' @param parameters the completed query parameters from igdb_parameters
+#' @param key IGDB API key
 #' @return a structure containing: \itemize{
 #' \item content: Parsed JSON
 #' \item path: complete path
 #' \item response
 #' }
 #'
-igdb_request <- function(endpoint, parameters = "") {
+igdb_request <- function(endpoint, parameters = "", key = "") {
   completePath <- paste(endpoint, parameters, sep = "")
   igdb_complete_request(completePath)
 }
@@ -24,20 +23,21 @@ igdb_request <- function(endpoint, parameters = "") {
 #' This function is used by igdb_request() which creates the complete path for you.
 #'
 #' @param completePath the complete pathcosists of the endpoint path and the query parameters
+#' @param key IGDB API key
 #' @return a structure containing: \itemize{
 #' \item content: Parsed JSON
 #' \item path: complete path
 #' \item response
 #' }
-igdb_complete_request <- function(completePath = "") {
+igdb_complete_request <- function(completePath = "", key = "") {
 
-  if(igdb_key == "") {
-    stop("Empty API key, please set the variable 'igdb_key' with your API key.")
+  if(key == "") {
+    stop("Empty API key, please set the parameter 'key' with your API key.")
   }
 
   url <- modify_url("http://api-endpoint.igdb.com", path = completePath)
 
-    resp <- GET(url, add_headers(`user-key` = igdb_key), encode = "json")
+    resp <- GET(url, add_headers(`user-key` = key), encode = "json")
 
   if (http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
